ccab22f0315472f311e321309f2310c00eb03726
samp101 <shmpos@gmail.com>
2022-09-15 21:01:48 +0300

    First Commit

---
+++ server/connections/db.js
@@ -0,0 +1,14 @@
+const knex = require('knex')
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+const blog_db = knex({
+    connection:{
+  host : process.env.DB_HOST,
+  port : process.env.DB_PORT,
+  user : process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+    }
+}) 
